f96e052c0a1c48cc423f656aa15ead2cb6dbbf5c
Valerio Borioni <valeriob@gmail.com>
2018-06-04 14:51:42 +0200

    JsonTransform command line

---
+++ src/JsonTransform/ConsoleJsonTransformationLogger.cs
@@ -0,0 +1,55 @@
+﻿namespace JsonTransform
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.VisualStudio.Jdt;
+
+    public class ConsoleJsonTransformationLogger : IJsonTransformationLogger
+    {
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string message, string fileName, int lineNumber, int linePosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogErrorFromException(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogErrorFromException(Exception ex, string fileName, int lineNumber, int linePosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogMessage(string message, string fileName, int lineNumber, int linePosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogWarning(string message, string fileName, int lineNumber, int linePosition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/JsonTransform/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace JsonTransform
+{
+    using System;
+    using System.IO;
+    using Microsoft.VisualStudio.Jdt;
+    using PowerArgs;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var parsed = Args.Parse<JsonTransformArguments>(args);
+                //Console.WriteLine("You entered string '{0}' and int '{1}'", parsed.StringArg, parsed.IntArg);
+                var logger = new ConsoleJsonTransformationLogger();
+                var transform = new JsonTransformation(parsed.TransformFile, logger);
+
+                if (File.Exists(parsed.DestinationFile))
+                    File.Delete(parsed.DestinationFile);
+                    
+                //File.Copy(parsed.SourceFile, parsed.DestinationFile);
+                var result = transform.Apply(parsed.SourceFile);
+                using (var destFile = File.OpenWrite(parsed.DestinationFile))
+                    result.CopyTo(destFile);
+
+                Console.WriteLine($"Transform completed successfully into {parsed.DestinationFile}");
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<JsonTransformArguments>());
+            }
+
+         
+            
+        }
+    }
+
+    public class JsonTransformArguments
+    {
+
+        [ArgRequired]
+        [ArgExistingFile]
+        [ArgDescription("Source")]
+        [ArgShortcut("s")]
+        public string SourceFile { get; set; }
+
+        [ArgRequired]
+        [ArgExistingFile]
+        [ArgDescription("Transform")]
+        [ArgShortcut("t")]
+        public string TransformFile { get; set; }
+
+        [ArgRequired]
+        [ArgDescription("Destination")]
+        [ArgShortcut("d")]
+        public string DestinationFile { get; set; }
+
+        //// This argument is required and if not specified the user will 
+        //// be prompted.
+        //[ArgRequired(PromptIfMissing = true)]
+
+        //public string StringArg { get; set; }
+
+        //// This argument is not required, but if specified must be >= 0 and <= 60
+        //[ArgRange(0, 60)]
+        //public int IntArg { get; set; }
+    }
+}
